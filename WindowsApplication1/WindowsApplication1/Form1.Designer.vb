843c2c4319ddac94485aa984c7b21313534eef9b
Estudiante <Estudiante@1303-15>
2017-02-02 20:29:58 -0600

    Creación de proyecto

--- WindowsApplication1/WindowsApplication1/Form1.Designer.vb
+++ WindowsApplication1/WindowsApplication1/Form1.Designer.vb
@@ -22,9 +22,113 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.cbxFigura = New System.Windows.Forms.ComboBox()
+        Me.lb1 = New System.Windows.Forms.Label()
+        Me.lbLadoA = New System.Windows.Forms.Label()
+        Me.lbLadoB = New System.Windows.Forms.Label()
+        Me.txtLadoA = New System.Windows.Forms.TextBox()
+        Me.txtLadoB = New System.Windows.Forms.TextBox()
+        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.txtResultado = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'cbxFigura
+        '
+        Me.cbxFigura.FormattingEnabled = True
+        Me.cbxFigura.Items.AddRange(New Object() {"Cuadrado", "Rectángulo", "Triángulo", "Paralelogramo"})
+        Me.cbxFigura.Location = New System.Drawing.Point(62, 72)
+        Me.cbxFigura.Name = "cbxFigura"
+        Me.cbxFigura.Size = New System.Drawing.Size(121, 21)
+        Me.cbxFigura.TabIndex = 0
+        '
+        'lb1
+        '
+        Me.lb1.AutoSize = True
+        Me.lb1.Location = New System.Drawing.Point(59, 35)
+        Me.lb1.Name = "lb1"
+        Me.lb1.Size = New System.Drawing.Size(155, 13)
+        Me.lb1.TabIndex = 1
+        Me.lb1.Text = "Seleccione la figura a calcular: "
+        '
+        'lbLadoA
+        '
+        Me.lbLadoA.AutoSize = True
+        Me.lbLadoA.Location = New System.Drawing.Point(59, 118)
+        Me.lbLadoA.Name = "lbLadoA"
+        Me.lbLadoA.Size = New System.Drawing.Size(39, 13)
+        Me.lbLadoA.TabIndex = 2
+        Me.lbLadoA.Text = "Label2"
+        Me.lbLadoA.Visible = False
+        '
+        'lbLadoB
+        '
+        Me.lbLadoB.AutoSize = True
+        Me.lbLadoB.Location = New System.Drawing.Point(209, 118)
+        Me.lbLadoB.Name = "lbLadoB"
+        Me.lbLadoB.Size = New System.Drawing.Size(39, 13)
+        Me.lbLadoB.TabIndex = 3
+        Me.lbLadoB.Text = "Label3"
+        Me.lbLadoB.Visible = False
+        '
+        'txtLadoA
+        '
+        Me.txtLadoA.Location = New System.Drawing.Point(58, 150)
+        Me.txtLadoA.Name = "txtLadoA"
+        Me.txtLadoA.Size = New System.Drawing.Size(71, 20)
+        Me.txtLadoA.TabIndex = 4
+        Me.txtLadoA.Visible = False
+        '
+        'txtLadoB
+        '
+        Me.txtLadoB.Location = New System.Drawing.Point(212, 150)
+        Me.txtLadoB.Name = "txtLadoB"
+        Me.txtLadoB.Size = New System.Drawing.Size(65, 20)
+        Me.txtLadoB.TabIndex = 5
+        Me.txtLadoB.Visible = False
+        '
+        'btnCalcular
+        '
+        Me.btnCalcular.Location = New System.Drawing.Point(58, 200)
+        Me.btnCalcular.Name = "btnCalcular"
+        Me.btnCalcular.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalcular.TabIndex = 6
+        Me.btnCalcular.Text = "Calcular"
+        Me.btnCalcular.UseVisualStyleBackColor = True
+        '
+        'txtResultado
+        '
+        Me.txtResultado.Location = New System.Drawing.Point(58, 245)
+        Me.txtResultado.Name = "txtResultado"
+        Me.txtResultado.Size = New System.Drawing.Size(234, 20)
+        Me.txtResultado.TabIndex = 7
+        Me.txtResultado.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(353, 293)
+        Me.Controls.Add(Me.txtResultado)
+        Me.Controls.Add(Me.btnCalcular)
+        Me.Controls.Add(Me.txtLadoB)
+        Me.Controls.Add(Me.txtLadoA)
+        Me.Controls.Add(Me.lbLadoB)
+        Me.Controls.Add(Me.lbLadoA)
+        Me.Controls.Add(Me.lb1)
+        Me.Controls.Add(Me.cbxFigura)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents cbxFigura As ComboBox
+    Friend WithEvents lb1 As Label
+    Friend WithEvents lbLadoA As Label
+    Friend WithEvents lbLadoB As Label
+    Friend WithEvents txtLadoA As TextBox
+    Friend WithEvents txtLadoB As TextBox
+    Friend WithEvents btnCalcular As Button
+    Friend WithEvents txtResultado As TextBox
 End Class
--- WindowsApplication1/WindowsApplication1/Form1.vb
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -1,3 +1,76 @@
 ﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lb1.Click
 
+    End Sub
+
+    Private Sub cbxFigura_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbxFigura.SelectedIndexChanged
+        Dim figura As Integer
+        figura = cbxFigura.SelectedIndex
+
+        Select Case figura
+            Case 0
+                lbLadoA.Visible = True
+                lbLadoA.Text = "Lado"
+                txtLadoA.Visible = True
+                txtLadoA.Text = "0"
+                lbLadoB.Visible = False
+                txtLadoB.Visible = False
+            Case 1
+                lbLadoA.Visible = True
+                lbLadoA.Text = "Largo"
+                txtLadoA.Visible = True
+                txtLadoA.Text = "0"
+                lbLadoB.Text = "Ancho"
+                lbLadoB.Visible = True
+                txtLadoB.Text = "0"
+                txtLadoB.Visible = True
+
+
+            Case 2
+                lbLadoA.Visible = True
+                lbLadoA.Text = "Base"
+                txtLadoA.Visible = True
+                txtLadoA.Text = "0"
+                lbLadoB.Visible = True
+                lbLadoB.Text = "Altura"
+                txtLadoB.Visible = True
+                txtLadoB.Text = "0"
+
+            Case 3
+                lbLadoA.Visible = True
+                lbLadoA.Text = "Base"
+                txtLadoA.Visible = True
+                txtLadoA.Text = "0"
+                lbLadoB.Visible = True
+                lbLadoB.Text = "Altura"
+                txtLadoB.Visible = True
+                txtLadoB.Text = "0"
+
+        End Select
+
+    End Sub
+
+    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        Dim calcular As Integer
+        calcular = cbxFigura.SelectedIndex()
+        Dim area As Integer
+
+        Select Case calcular
+            Case 0
+                area = Int(txtLadoA.Text) * Int(txtLadoA.Text)
+
+            Case 1
+                area = Int(txtLadoA.Text) * Int(txtLadoB.Text)
+
+            Case 2
+                area = (Int(txtLadoA.Text) * Int(txtLadoB.Text)) / 2
+
+            Case 3
+                area = Int(txtLadoA.Text) * Int(txtLadoB.Text)
+
+        End Select
+
+        txtResultado.Visible = True
+        txtResultado.Text = "El resultado es: " & area
+    End Sub
 End Class
